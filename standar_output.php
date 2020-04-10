7f503e4bc087d7cc1353eda0a8151a8a1e6ff2ab
RidhoAldiko <ridhoaldiko@student.uir.ac.id>
2020-04-10 08:29:52 +0700

    menambahkan standar output

---
+++ standar_output.php
@@ -0,0 +1,15 @@
+<?php
+//echo dapat mencetak lebih dari satu data contoh :
+echo 'Alfazzasoftware',1,2,3,4;
+//print hanya dapat mencetak data tunggal(hanya satu data) contoh :
+print 'Alfazzasoftware';
+//print_r digunakan untuk mencetak isi array contoh :
+print_r('Alfazzasoftware');
+/*var_dump digunakan untuk melihat isi variabel beserta 
+informasi dari variabel tersebut contoh */
+var_dump('Alfazzasoftware');
+/*perlu diketahui print_r dan var_dump sering 
+digunakan untuk proses debugging*/
+?>
+
+
