a702b39c40dc9511351e847b8fc83da5c0dd7dda
RidhoAldiko <ridhoaldiko@student.uir.ac.id>
2020-04-13 08:16:56 +0700

    menambahkan penggunaan kutip

---
+++ penggunaan_kutip.php
@@ -0,0 +1,11 @@
+<?php
+//Menampilkan isi variabel
+$nama = "Ridho Aldiko";
+echo "Halo nama saya $nama"; //<--- Benar
+echo 'Halo nama saya $nama'; //<--- Salah
+//Menampilkan String
+echo "jum'at"; //<--- Benar 
+echo 'jum'at';  //<--- Salah
+
+
+
