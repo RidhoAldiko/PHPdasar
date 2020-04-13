2df2c7f069b90cbc53a8ca3c6444d87f8330b4c7
RidhoAldiko <ridhoaldiko@student.uir.ac.id>
2020-04-14 00:20:25 +0700

    menambahkan operator penugasan

---
+++ operator_penugasan.php
@@ -0,0 +1,24 @@
+<?php
+// =, +=, -=, *=, /=, %=, .=
+$a = 10; echo $a; //penugasan sama dengan 
+$a = 10; $a += 7; //penugasan penjumlahan
+echo $a;
+$a = 10; $a -= 7; //penugasan pengurangan
+echo $a;
+$a = 10; $a *= 7; //penugasan perkalian
+echo $a;
+$a = 10; $a /= 7; //penugasan pembagian 
+echo $a;
+$a = 10; $a %= 7; //penugasan sisa bagi 
+echo $a;
+//penugasan penggabung string(concat) 
+$nama = "Ridho"; $nama .= "Aldiko"; 
+echo $nama;
+?>
+
+
+
+
+
+
+
