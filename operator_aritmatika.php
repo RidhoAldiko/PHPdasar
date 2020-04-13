89ff3f243a819e38e7a1e7ad663905b968cea828
RidhoAldiko <ridhoaldiko@student.uir.ac.id>
2020-04-13 23:19:58 +0700

    menambahkan operator aritmatika

---
+++ operator_aritmatika.php
@@ -0,0 +1,14 @@
+<?php
+//bisa operasikan langsung, contoh :
+echo 10 + 7 ;
+//atau disimpan kedalam variabel, contoh :
+$a = 10;
+$b = 7;
+echo $a + $b; //penjumlahan
+echo $a - $b; //pengurangan
+echo $a * $b; //perkalian
+echo $a / $b; //pembagian
+echo $a % $b; //sisa bagi
+?>
+
+
