42cda3dcc1a7125e9d5ad52b3ec9bfb439b58567
RidhoAldiko <ridhoaldiko@student.uir.ac.id>
2020-04-14 00:39:42 +0700

    menambahkan operator perbandingan

---
+++ operator_perbandingan.php
@@ -0,0 +1,13 @@
+<?php
+/* disimbolkan dengan <, >, <=, >=, ==, != */
+/* operator perbandingan akan menghasilkan
+output boolean(true or false) contoh : */
+$a = 10; $b = 7;
+var_dump($a >= $b);
+/* operator perbandingan hanya membandingkan
+nilai tanpa membandingkan tipe datanya contoh : */
+$a = 10; $b = "10"; //variabel b adalah string
+var_dump($a == $b);
+?>
+
+
